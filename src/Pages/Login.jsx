2f828aa243e5fee81020bdcf92303e69f2ea2191
iniouwa <inioluwaosibajo@gmail.com>
2025-06-05 10:24:24 +0100

    login as guest

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -6,29 +6,28 @@ import { login } from "../services";
 
 
 export const Login = () => {
-  useTitle("Login")
+  useTitle("Login");
   const navigate = useNavigate();
   const email = useRef();
   const password = useRef();
 
-  async function handleLogin(event){
+  async function handleLogin(event) {
     event.preventDefault();
-    try{
-       const authDetail = {
-      email: email.current.value,
-      password: password.current.value
-    }
-    const data = await login(authDetail);
-    data.accessToken ? navigate("/products") : toast.error(data);
-
-    }catch(error){
-      if(error.status === 401){
+    try {
+      const authDetail = {
+        email: email.current.value,
+        password: password.current.value
+      };
+      const data = await login(authDetail);
+      data.accessToken ? navigate("/products") : toast.error(data);
+    } catch (error) {
+      if (error.status === 401) {
         toast.error("Invalid email or password", {
           closeButton: true,
           autoClose: 5000,
           closeOnClick: true
         });
-      }else{
+      } else {
         toast.error(error.message, {
           closeButton: true,
           autoClose: 5000,
@@ -37,27 +36,27 @@ export const Login = () => {
       }
       console.error("Login failed:", error);
     }
-   
   }
 
-  async function handleLoginGuest(){
-    email.current.value = "danny@gmail";
-    password.current.value = "12345678";
-    try{
+  async function handleLoginGuest() {
+    email.current.value = import.meta.env.VITE_GUEST;
+    password.current.value = import.meta.env.VITE_PASSWORD;
+
+    try {
       const authDetail = {
         email: email.current.value,
         password: password.current.value
-      }
+      };
       const data = await login(authDetail);
       data.accessToken ? navigate("/products") : toast.error(data);
-    } catch(error) {
-      if(error.status === 401){
+    } catch (error) {
+      if (error.status === 401) {
         toast.error("Invalid email or password", {
           closeButton: true,
           autoClose: 5000,
           closeOnClick: true
         });
-      }else{
+      } else {
         toast.error(error.message, {
           closeButton: true,
           autoClose: 5000,
@@ -72,19 +71,19 @@ export const Login = () => {
     <main>
       <section>
         <p className="text-2xl text-center font-semibold dark:text-slate-100 my-10 underline underline-offset-8">Login</p>
-      </section>        
-        <form onSubmit={handleLogin}>
-          <div className="mb-6">
-              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-              <input ref={email} type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="danny@example.com" required autoComplete="off" />
-          </div>
-          <div className="mb-6">
-              <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
-              <input ref={password} type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-          </div>
-          <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</button>
-        </form>
-        <button onClick={()=>handleLoginGuest()} className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button> 
+      </section>
+      <form onSubmit={handleLogin}>
+        <div className="mb-6">
+          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+          <input ref={email} type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="danny@example.com" required autoComplete="off" />
+        </div>
+        <div className="mb-6">
+          <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
+          <input ref={password} type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+        </div>
+        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</button>
+      </form>
+      <button onClick={handleLoginGuest} className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button>
     </main>
-  )
-}
+  );
+};
