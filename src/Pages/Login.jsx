4afd493deea2a6f6f86be5452f4d396bc7638cea
iniouwa <inioluwaosibajo@gmail.com>
2025-06-05 10:14:48 +0100

    login as guest

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -40,6 +40,34 @@ export const Login = () => {
    
   }
 
+  async function handleLoginGuest(){
+    email.current.value = "danny@gmail";
+    password.current.value = "12345678";
+    try{
+      const authDetail = {
+        email: email.current.value,
+        password: password.current.value
+      }
+      const data = await login(authDetail);
+      data.accessToken ? navigate("/products") : toast.error(data);
+    } catch(error) {
+      if(error.status === 401){
+        toast.error("Invalid email or password", {
+          closeButton: true,
+          autoClose: 5000,
+          closeOnClick: true
+        });
+      }else{
+        toast.error(error.message, {
+          closeButton: true,
+          autoClose: 5000,
+          closeOnClick: true
+        });
+      }
+      console.error("Login failed:", error);
+    }
+  }
+
   return (
     <main>
       <section>
@@ -56,7 +84,7 @@ export const Login = () => {
           </div>
           <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</button>
         </form>
-        {/* <button className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button> */}
+        <button onClick={()=>handleLoginGuest()} className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button> 
     </main>
   )
 }
